20eb108dc9ec925dce3834b09ad7ad16f9cb9528
TeddyantoIJ <teddyanto.ij@gmail.com>
2021-07-22 21:41:51 +0700

    ini apakek

--- src/main/java/com/example/ga/kelompok11/repository/DetailPeminjamanRepositoy.java
+++ src/main/java/com/example/ga/kelompok11/repository/DetailPeminjamanRepositoy.java
@@ -1,4 +1,7 @@
 package com.example.ga.kelompok11.repository;
 
-public interface DetailPeminjamanRepositoy {
+import com.example.ga.kelompok11.model.DetailPeminjaman;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DetailPeminjamanRepositoy extends CrudRepository<DetailPeminjaman, Integer> {
 }
---
+++ src/main/java/com/example/ga/kelompok11/service/Detail_peminjaman_service.java
@@ -0,0 +1,36 @@
+package com.example.ga.kelompok11.service;
+
+import com.example.ga.kelompok11.model.DetailPeminjaman;
+import com.example.ga.kelompok11.repository.DetailPeminjamanRepositoy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Detail_peminjaman_service {
+    @Autowired
+    DetailPeminjamanRepositoy detailPeminjamanRepositoy;
+
+    public List<DetailPeminjaman> getAll(){
+        List<DetailPeminjaman> a = (List<DetailPeminjaman>) detailPeminjamanRepositoy.findAll();
+        return  a;
+    }
+
+    public List<DetailPeminjaman> getAllByIdPeminjaman(String idPeminjaman){
+        List<DetailPeminjaman> a = getAll();
+        List<DetailPeminjaman> output = new ArrayList<>();
+        for(DetailPeminjaman b : a){
+            if(b.getIdPeminjaman().equals(idPeminjaman)){
+                output.add(b);
+            }
+        }
+        return output;
+    }
+
+//    ====================================================
+    public void save(DetailPeminjaman dt){
+        detailPeminjamanRepositoy.save(dt);
+    }
+}
